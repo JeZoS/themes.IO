5c27166c1a268a00ee4a9c0457fdf3f9734a315c
JeZoS <prabhat262111@gmail.com>
2021-03-15 16:35:05 +0530

    starter_backend

---
+++ controllers/posts.js
@@ -0,0 +1,28 @@
+const Theme = require("../models/themes");
+
+const getAllPosts = async (req, res) => {
+  try {
+    const allThemes = await Theme.find();
+    res.status(200).json(allThemes);
+  } catch (err) {
+    res.status(404).json({
+      message: err.message,
+    });
+  }
+};
+
+const createPost = (req, res) => {
+  try {
+      const {title,description,}
+  } catch (err) {
+    res.status(404).json({
+      message: err.message,
+    });
+  }
+};
+
+const editPost = (req, res) => {};
+
+const deletePost = (req, res) => {};
+
+module.exports = { getAllPosts, createPost, editPost, deletePost };
